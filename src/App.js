8ec46883e477900c4d03ae1ab2beecf4af5cb554
OMAR-GODDI <73104649+OMAR-GODDI@users.noreply.github.com>
2020-11-11 22:12:41 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import Form from "./cards/form";
+
+
+
+
+function App() {
+return (
+  
+  <div>
+    <div className="button">
+    <button type="submit" className="bout btn btn-primary ">SIGN UP</button>
+    <button type="submit" className="bout2 btn btn-primary ">LOGIN</button>
+    </div>
+  <h1 className="form-title">CONTACT ME</h1>
+  <Form/>
+  </div>
+  
+)
+    
+  }
+
+export default App;
